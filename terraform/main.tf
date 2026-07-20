6dc02e122bdaf72228ece6a2bae17e8478345ba1
magley <60552482+magley@users.noreply.github.com>
2026-07-20 12:33:49 +0200

    fix: Remove issues in terraform files' format

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,20 @@
 resource "helm_release" "charts" {
-    for_each = var.helm_charts
-    
-    name      = coalesce(each.value.name, each.key)
-    chart     = each.value.chart
-    version   = var.helm_chart_versions[each.key]
-    namespace = var.environment
-    create_namespace = true
+  for_each = var.helm_charts
 
-    set = [
-        {
-            name  = "environment"
-            value = var.environment
-        },
-        {
-            name  = "chart.namespace"
-            value = var.k8s_namespace
-        },
-    ]
+  name             = coalesce(each.value.name, each.key)
+  chart            = each.value.chart
+  version          = var.helm_chart_versions[each.key]
+  namespace        = var.environment
+  create_namespace = true
+
+  set = [
+    {
+      name  = "environment"
+      value = var.environment
+    },
+    {
+      name  = "chart.namespace"
+      value = var.k8s_namespace
+    },
+  ]
 }
